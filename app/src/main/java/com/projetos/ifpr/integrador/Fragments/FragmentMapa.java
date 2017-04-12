f5a46d769ac92f5c43c232ae3a78b02fe30bf9f2
Jeremy <jeremy_rauber@live.com>
2017-04-12 17:33:42 -0300

    Adicionado os Fragments dos menus                                        Adicionado funçao de ligação (FUNFANDO)                                        cores ?

--- app/src/main/java/com/projetos/ifpr/integrador/Fragments/FragmentMapa.java
+++ app/src/main/java/com/projetos/ifpr/integrador/Fragments/FragmentMapa.java
@@ -2,31 +2,34 @@ package com.projetos.ifpr.integrador.Fragments;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
+
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.Context;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.maps.android.heatmaps.HeatmapTileProvider;
 import com.projetos.ifpr.integrador.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by jeremy on 30/03/2017.
@@ -34,6 +37,8 @@ import com.projetos.ifpr.integrador.R;
 public class FragmentMapa extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private HeatmapTileProvider mProvider;
+    private TileOverlay mOverlay;
 
     public static FragmentMapa newInstance() {
         FragmentMapa fragment = new FragmentMapa();
@@ -45,9 +50,31 @@ public class FragmentMapa extends Fragment implements OnMapReadyCallback {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_mapa, null, false);
 
+
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        List<LatLng> list = null;
+        // Get the data: latitude/longitude positions of police stations.
+        try {
+            list = readItems(R.raw.police);
+        } catch (JSONException e) {
+            Toast.makeText(this.getContext(), "Problem reading list of locations.", Toast.LENGTH_LONG).show();
+        }
+
+        // Create a heat map tile provider, passing it the latlngs of the police stations.
+
+        mProvider = new HeatmapTileProvider.Builder().data(list).build();
+
+        System.out.print("*******************passou no esquema quente0");
+
+        // Add a tile overlay to the map, using the heat map tile provider.
+        mOverlay =  mMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
+
+
+
+
 
         return view;
     }
@@ -62,13 +89,38 @@ public class FragmentMapa extends Fragment implements OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
 
+        mMap = googleMap;
         // Add a marker in Sydney and move the camera
-        LatLng FozdoIguacu = new LatLng(-25.5469,-54.5882);
+       // LatLng FozdoIguacu = new LatLng(-25.5469,-54.5882);
+
+        LatLng FozdoIguacu = new LatLng(-37.1886,145.708);
+
         mMap.addMarker(new MarkerOptions().position(FozdoIguacu).title("Marcador em Foz do Iguaçu"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(FozdoIguacu));
+
+    }
+
+    private void addHeatMap() {
+
+
+    }
+
+
+    private ArrayList<LatLng> readItems(int resource) throws JSONException {
+        ArrayList<LatLng> list = new ArrayList<>();
+        InputStream inputStream = getResources().openRawResource(resource);
+        String json = new Scanner(inputStream).useDelimiter("\\A").next();
+        JSONArray array = new JSONArray(json);
+        for (int i = 0; i < array.length(); i++) {
+            JSONObject object = array.getJSONObject(i);
+            double lat = object.getDouble("lat");
+            double lng = object.getDouble("lng");
+            list.add(new LatLng(lat, lng));
+        }
+        return list;
     }
 }
