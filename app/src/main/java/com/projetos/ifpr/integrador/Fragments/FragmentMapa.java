cdddaf29b93b624f3ca63a4fbc113bd44b9de85f
Jeremy <jeremy_rauber@live.com>
2017-04-13 17:12:15 -0300

    HeatMap Funcionando

--- app/src/main/java/com/projetos/ifpr/integrador/Fragments/FragmentMapa.java
+++ app/src/main/java/com/projetos/ifpr/integrador/Fragments/FragmentMapa.java
@@ -1,120 +1,246 @@
 package com.projetos.ifpr.integrador.Fragments;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-
 import com.google.android.gms.maps.model.MarkerOptions;
-
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
-
 import com.google.android.gms.maps.model.TileOverlay;
 import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.maps.android.heatmaps.Gradient;
 import com.google.maps.android.heatmaps.HeatmapTileProvider;
+import com.projetos.ifpr.integrador.Consulta;
+import com.projetos.ifpr.integrador.Inicial;
+import com.projetos.ifpr.integrador.MainActivity;
+import com.projetos.ifpr.integrador.PermissionUtils;
 import com.projetos.ifpr.integrador.R;
+import com.projetos.ifpr.integrador.Usuario;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
+import cz.msebera.android.httpclient.HttpResponse;
+import cz.msebera.android.httpclient.NameValuePair;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.entity.UrlEncodedFormEntity;
+import cz.msebera.android.httpclient.client.methods.HttpGet;
+import cz.msebera.android.httpclient.client.methods.HttpPost;
+import cz.msebera.android.httpclient.client.utils.URLEncodedUtils;
+import cz.msebera.android.httpclient.impl.client.HttpClientBuilder;
+import cz.msebera.android.httpclient.message.BasicNameValuePair;
+import cz.msebera.android.httpclient.util.EntityUtils;
+
 /**
- * Created by jeremy on 30/03/2017.
+ * Created by jeremy on 13/04/2017.
  */
-public class FragmentMapa extends Fragment implements OnMapReadyCallback {
+
+public class FragmentMapa extends FragmentActivity  implements OnMapReadyCallback{
+
+    /******** CONFIGURACOES PERSONALIZADAS PARA O HEATMAP ********/
+
+    /* Convolução radial alternativa pro raio*/
+    private static final int ALT_HEATMAP_RADIUS = 12;
+
+    /* Opacidade alternativa para a camada de heatmap */
+    private static final double ALT_HEATMAP_OPACITY = 0.5;
+
+    /* Troca de cor do gradiente para o heatmap (azul -> vermelho) */
+    private static final int[] ALT_HEATMAP_GRADIENT_COLORS = {
+            Color.argb(0, 0, 255, 255),// transparent
+            Color.argb(255 / 3 * 2, 0, 255, 255),
+            Color.rgb(0, 191, 255),
+            Color.rgb(0, 0, 127),
+            Color.rgb(255, 0, 0)
+    };
+
+    public static final float[] ALT_HEATMAP_GRADIENT_START_POINTS = {
+            0.0f, 0.10f, 0.20f, 0.60f, 1.0f
+    };
+
+    public static final Gradient ALT_HEATMAP_GRADIENT = new Gradient(ALT_HEATMAP_GRADIENT_COLORS,
+            ALT_HEATMAP_GRADIENT_START_POINTS);
+
 
     private GoogleMap mMap;
     private HeatmapTileProvider mProvider;
     private TileOverlay mOverlay;
-
-    public static FragmentMapa newInstance() {
-        FragmentMapa fragment = new FragmentMapa();
-        return fragment;
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_mapa, null, false);
-
-
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-
-        List<LatLng> list = null;
-        // Get the data: latitude/longitude positions of police stations.
-        try {
-            list = readItems(R.raw.police);
-        } catch (JSONException e) {
-            Toast.makeText(this.getContext(), "Problem reading list of locations.", Toast.LENGTH_LONG).show();
-        }
-
-        // Create a heat map tile provider, passing it the latlngs of the police stations.
-
-        mProvider = new HeatmapTileProvider.Builder().data(list).build();
-
-        System.out.print("*******************passou no esquema quente0");
-
-        // Add a tile overlay to the map, using the heat map tile provider.
-        mOverlay =  mMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
-
-
-
-
-
-        return view;
-    }
+    private boolean mDefaultGradient = true;
+    private boolean mDefaultRadius = true;
+    private boolean mDefaultOpacity = true;
+    private String pontosFromWebService;
+    private boolean variavelControleInternet;
+    LatLng myPosition;
 
 
     /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
+     * Maps name of data set to data (list of LatLngs)
+     * Also maps to the URL of the data set for attribution
      */
+    private HashMap<String, DataSet> mLists = new HashMap<String, DataSet>();
+
+    protected Location mLastLocation;
+
+    protected String mLatitudeLabel;
+    protected String mLongitudeLabel;
+    protected TextView mLatitudeText;
+    protected TextView mLongitudeText;
+    protected GoogleApiClient mGoogleApiClient;
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.heatmaps_demo);
 
-        mMap = googleMap;
-        // Add a marker in Sydney and move the camera
-       // LatLng FozdoIguacu = new LatLng(-25.5469,-54.5882);
+        ChamadaWeb chamada = new ChamadaWeb("http://10.0.2.2:8090/IntegradorWS/rest/servicos/consultaMapaCalor");
+        chamada.execute();
+    }
 
-        LatLng FozdoIguacu = new LatLng(-37.1886,145.708);
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(this,Inicial.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        startActivity(intent);
+    }
+
+    protected void startDemo() {
+
+        LatLng FozdoIguacu = new LatLng(-25.5469,-54.5882);
 
         mMap.addMarker(new MarkerOptions().position(FozdoIguacu).title("Marcador em Foz do Iguaçu"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(FozdoIguacu));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(FozdoIguacu, 10));
+
+
+        // Set up the spinner/dropdown list
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.heatmaps_datasets_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new SpinnerActivity());
+
+        try {
+            mLists.put(getString(R.string.foco), new DataSet(readItems(R.raw.police)));
+
+        } catch (JSONException e) {
+            Toast.makeText(this, "Problema lendo a lista de localização.", Toast.LENGTH_LONG).show();
+        }
+
+
 
     }
 
-    private void addHeatMap() {
-
-
+    public void changeRadius(View view) {
+        if (mDefaultRadius) {
+            mProvider.setRadius(ALT_HEATMAP_RADIUS);
+        } else {
+            mProvider.setRadius(HeatmapTileProvider.DEFAULT_RADIUS);
+        }
+        mOverlay.clearTileCache();
+        mDefaultRadius = !mDefaultRadius;
     }
 
+    public void changeGradient(View view) {
+        if (mDefaultGradient) {
+            mProvider.setGradient(ALT_HEATMAP_GRADIENT);
+        } else {
+            mProvider.setGradient(HeatmapTileProvider.DEFAULT_GRADIENT);
+        }
+        mOverlay.clearTileCache();
+        mDefaultGradient = !mDefaultGradient;
+    }
 
+    public void changeOpacity(View view) {
+        if (mDefaultOpacity) {
+            mProvider.setOpacity(ALT_HEATMAP_OPACITY);
+        } else {
+            mProvider.setOpacity(HeatmapTileProvider.DEFAULT_OPACITY);
+        }
+        mOverlay.clearTileCache();
+        mDefaultOpacity = !mDefaultOpacity;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        if (mMap != null) {
+            return;
+        }
+        mMap = map;
+        startDemo();
+            }
+
+
+
+    // Dealing with spinner choices
+    public class SpinnerActivity implements AdapterView.OnItemSelectedListener {
+        public void onItemSelected(AdapterView<?> parent, View view,
+                                   int pos, long id) {
+            String dataset = parent.getItemAtPosition(pos).toString();
+
+            if(variavelControleInternet) {
+                // Check if need to instantiate (avoid setData etc twice)
+                if (mProvider == null) {
+                    mProvider = new HeatmapTileProvider.Builder().data(
+                            mLists.get(getString(R.string.foco)).getData()).build();
+                    mOverlay = mMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
+                    // Render links
+                } else {
+                    mProvider.setData(mLists.get(dataset).getData());
+                    mOverlay.clearTileCache();
+                }
+            }
+
+        }
+
+        public void onNothingSelected(AdapterView<?> parent) {
+            // Another interface callback
+        }
+    }
+
+    // Datasets from http://data.gov.au
     private ArrayList<LatLng> readItems(int resource) throws JSONException {
-        ArrayList<LatLng> list = new ArrayList<>();
-        InputStream inputStream = getResources().openRawResource(resource);
-        String json = new Scanner(inputStream).useDelimiter("\\A").next();
-        JSONArray array = new JSONArray(json);
+        ArrayList<LatLng> list = new ArrayList<LatLng>();
+        //InputStream inputStream = getResources().openRawResource(R.raw.police);
+
+       // String json = new Scanner(inputStream).useDelimiter("\\A").next();
+
+        JSONArray array = new JSONArray(pontosFromWebService);
+
         for (int i = 0; i < array.length(); i++) {
             JSONObject object = array.getJSONObject(i);
             double lat = object.getDouble("lat");
@@ -123,4 +249,62 @@ public class FragmentMapa extends Fragment implements OnMapReadyCallback {
         }
         return list;
     }
+
+    private class DataSet {
+        private ArrayList<LatLng> mDataset;
+
+        public DataSet(ArrayList<LatLng> dataSet) {
+            this.mDataset = dataSet;
+        }
+
+        public ArrayList<LatLng> getData() {
+            return mDataset;
+        }
+
+    }
+
+    private void setUpMap() {
+        ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMapAsync(this);
+    }
+
+
+    private class ChamadaWeb extends AsyncTask<String, Void, String> {
+        private String enderecoWeb;
+        private String idUsuario;
+
+        public ChamadaWeb(String endereco) {
+            enderecoWeb = endereco;
+        }
+        @Override
+        protected String doInBackground(String... params) {
+            HttpClient cliente = HttpClientBuilder.create().build();
+
+            try {
+                System.out.println(enderecoWeb);
+                HttpGet chamada = new HttpGet(enderecoWeb);
+                HttpResponse resposta = cliente.execute(chamada);
+                return EntityUtils.toString(resposta.getEntity());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        public void onPostExecute(String resultado) {
+            if (resultado != null) {
+                pontosFromWebService = resultado;
+                    System.out.println(resultado);
+                    setUpMap();
+                    variavelControleInternet = true;
+                Toast.makeText(FragmentMapa.this, "Usando banco de dados online!", Toast.LENGTH_SHORT).show();
+            }else{
+                InputStream inputStream = getResources().openRawResource(R.raw.police);
+                pontosFromWebService =new Scanner(inputStream).useDelimiter("\\A").next();
+
+                variavelControleInternet = false;
+                Toast.makeText(FragmentMapa.this, "Não foi possível conectar com o banco de dados, verifique sua conexão e tente novamente", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/projetos/ifpr/integrador/Inicial.java
+++ app/src/main/java/com/projetos/ifpr/integrador/Inicial.java
@@ -6,31 +6,25 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 
 import com.projetos.ifpr.integrador.Fragments.FragmentBuscar;
-import com.projetos.ifpr.integrador.Fragments.FragmentChamadas;
 import com.projetos.ifpr.integrador.Fragments.FragmentEditar;
 import com.projetos.ifpr.integrador.Fragments.FragmentMapa;
 import com.projetos.ifpr.integrador.Fragments.FragmentPreferencias;
 
 public class Inicial extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, FragmentBuscar.OnFragmentInteractionListener {
+        implements NavigationView.OnNavigationItemSelectedListener {
 
 
     private static final int PERMISSIONS_REQUEST_PHONE_CALL = 100;
@@ -43,32 +37,6 @@ public class Inicial extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-        /* botao que tem tres pontos...
-        if (savedInstanceState == null) {
-            FragmentEditar fragment = null;
-            Class fragmentClass = null;
-            fragmentClass = FragmentOne.class;
-            try {
-                fragment = (FragmentEditar) fragmentClass.newInstance();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
-        }
-
-        // INICIALIZACAO menubar LATERAL
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        */
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -84,34 +52,11 @@ public class Inicial extends AppCompatActivity
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
+        drawer.closeDrawer(GravityCompat.START);
+    } else {
+        super.onBackPressed();
     }
-/* menu que não será usado
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }*/
-
+}
 
     // ACOES NO MENUBAR LATERAL
     @SuppressWarnings("StatementWithEmptyBody")
@@ -129,11 +74,13 @@ public class Inicial extends AppCompatActivity
         } else if (id == R.id.nav_preferencias) {
             fragmentClass = FragmentPreferencias.class;
         } else if (id == R.id.nav_mapa) {
-            fragmentClass = FragmentMapa.class;
+            Intent intent = new Intent(this, FragmentMapa.class);
+            finish();
+            startActivity(intent);
+
         } else if (id == R.id.nav_call) {
            // fragmentClass = FragmentChamadas.class; acho q nao precisa abrir novo fragment
             call();
-
         }
 
         try {
@@ -145,6 +92,7 @@ public class Inicial extends AppCompatActivity
             FragmentManager fragmentManager = getSupportFragmentManager();
             fragmentManager.beginTransaction()
                     .replace(R.id.flContent, fragment)
+                    .addToBackStack(null)
                     .commit();
         }
 
@@ -153,10 +101,7 @@ public class Inicial extends AppCompatActivity
         return true;
     }
 
-    @Override
-    public void onFragmentInteraction(Uri uri) {
 
-    }
 
     // DOIDERA PARA FAZER LIGACAUM
 
